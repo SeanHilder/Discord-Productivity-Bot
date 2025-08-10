c4dec97e8cd20e51af4dd05c1979610adf15d36d
Sean Hilder <seanhilder42@gmail.com>
2025-08-10 18:23:50 +1000

    Initial code foundation for bot

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
